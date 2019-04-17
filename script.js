1b380bdd593ec1f12c8ab554989463d9579c106c
Eric Wittenberg <ebwittenberg@gmail.com>
2019-04-17 15:03:16 -0400

    adds fixed nav bar when scrolling

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const navBar = document.querySelector('[data-nav]');
 
+
+window.addEventListener('scroll', function() {
+    // if at top of the page
+    if (window.pageYOffset === 0) {
+        navBar.classList.remove('fixed');
+    } else {
+        navBar.classList.add('fixed')
+    }
+})
